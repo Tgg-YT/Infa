9614a475a9ac2f5893fcc2f2493e7a8f0d32859f
Tgg-YT <konradek.derewecki@gmail.com>
2022-03-10 09:14:53 +0100

    while

--- liczby pierwsze/liczby pierwsze.cpp
+++ liczby pierwsze/liczby pierwsze.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
     int p = 2;
     int d = 0;
     int lp = 0;
     int n = 0;
+    bool k8 = true;
+
+    while (lp < n) {
+        for (int d = 2; p - 1; ) {
+            if (p % d)
+            {
+
+            }
+        }
+    }
 }
